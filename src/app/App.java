0f92950a00a29310214caf847fd066aadf54edb9
Gordhan <kgordhan@dxc.com>
2020-04-02 13:16:11 +1300

    Problem 11, Likely not the best solution, Quite messy

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(SummationOfPrimes.calculate(2000000));
+        System.out.println(LargestInGrid.calculate());
     }
 }
